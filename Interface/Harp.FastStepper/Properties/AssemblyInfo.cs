b4f05aa451e4d15b37339a3347e70281954f36ed
glopesdev <glopesdev@users.noreply.github.com>
2025-01-24 13:52:45 +0000

    Update package metadata

--- Interface/Harp.FastStepper/Properties/AssemblyInfo.cs
+++ Interface/Harp.FastStepper/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: XmlNamespacePrefix("clr-namespace:Harp.FastStepper", "vh2")]
+[assembly: XmlNamespacePrefix("clr-namespace:Harp.FastStepper", "fstep")]
 [assembly: WorkflowNamespaceIcon("Bonsai:ElementIcon.Daq")]
